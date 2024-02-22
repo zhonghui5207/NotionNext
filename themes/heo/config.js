f0885255c00588a87b77962a9aea56b8ee8d3d95
Ryan <66513193+zhonghui5207@users.noreply.github.com>
2024-02-22 21:38:21 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2021-01-04', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
@@ -23,9 +23,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: '/shop',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '付费教程', url: '/tag/付费教程' },
+  HEO_HERO_CATEGORY_1: { title: '我的小店', url: '/shop' },
+  HEO_HERO_CATEGORY_2: { title: '付费教程', url: '/tag/付费教程' },
+  HEO_HERO_CATEGORY_3: { title: '热门文章', url: '/tag/热门文章' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
