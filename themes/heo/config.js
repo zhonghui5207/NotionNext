18cc2e082930424de207b3f8d72e13cb6aeb26cf
Ryan <66513193+zhonghui5207@users.noreply.github.com>
2024-02-22 14:37:29 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,9 +23,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: '/shop',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/bikan' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/remen' },
-  HEO_HERO_CATEGORY_3: { title: '付费教程', url: '/tag/fufei' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '付费教程', url: '/tag/付费教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
